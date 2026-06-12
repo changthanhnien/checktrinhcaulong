40f137c03109660270a37bd05052eaeabda5d95c
changthanhnien <140770880+changthanhnien@users.noreply.github.com>
2026-06-12 11:23:31 +0700

    Fix avatar image loading and move favicon to public

--- src/utils/avatars.ts
+++ src/utils/avatars.ts
@@ -8,14 +8,21 @@ export interface AvatarItem {
 }
 
 // Local image assets
-export const FLAGSHIP_MALE = "/src/assets/images/chibi_male_champion_1781233138706.jpg";
-export const FLAGSHIP_FEMALE = "/src/assets/images/chibi_female_champion_1781233150553.jpg";
+import flagshipMaleImg from "../assets/images/chibi_male_champion_1781233138706.jpg";
+import flagshipFemaleImg from "../assets/images/chibi_female_champion_1781233150553.jpg";
+import maleSmashImg from "../assets/images/chibi_male_smash_1781234025890.jpg";
+import maleDefendImg from "../assets/images/chibi_male_defend_1781234039098.jpg";
+import femaleSmashImg from "../assets/images/chibi_female_smash_1781234053238.jpg";
+import femaleDefendImg from "../assets/images/chibi_female_defend_1781234070955.jpg";
 
-export const MALE_SMASH = "/src/assets/images/chibi_male_smash_1781234025890.jpg";
-export const MALE_DEFEND = "/src/assets/images/chibi_male_defend_1781234039098.jpg";
+export const FLAGSHIP_MALE = flagshipMaleImg;
+export const FLAGSHIP_FEMALE = flagshipFemaleImg;
 
-export const FEMALE_SMASH = "/src/assets/images/chibi_female_smash_1781234053238.jpg";
-export const FEMALE_DEFEND = "/src/assets/images/chibi_female_defend_1781234070955.jpg";
+export const MALE_SMASH = maleSmashImg;
+export const MALE_DEFEND = maleDefendImg;
+
+export const FEMALE_SMASH = femaleSmashImg;
+export const FEMALE_DEFEND = femaleDefendImg;
 
 export const getAvatars = (): AvatarItem[] => {
   return [
